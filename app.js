d5e3bbcb605471bc8913471b9c62d8a62a0807a1
NithinRegidi <regidinithinsainaidu@gmail.com>
2025-09-07 21:07:56 +0530

    Basic CRUD operations are Done

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const mongoose = require('mongoose');
 const MONGO_URL = "mongodb://127.0.0.1:27017/BoardOn";
 const Listing = require("./models/listing");
 const path = require("path");
+const methodOverride = require("method-override");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended : true }));
+app.use(methodOverride("_method"));
 
 // MongoDB Database Connected
 main()
@@ -35,14 +38,50 @@ app.get("/listings", async (req, res) => {
 })
 
 
+// New Listing Route
+app.get("/listings/new", (req, res) =>{
+    res.render("new.ejs");
+})
+
+// Show Route 
+app.get("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    const listingById = await Listing.findById(id);
+    res.render("show.ejs", {listings : listingById});
+})
 
 
+// create route
+app.post("/listings", async (req, res) => {
+    const newListing = new Listing(req.body);
+    await newListing.save();
+    res.redirect("/listings");
+    console.log("New listing added");
+});
 
 
+//edit listing
+app.get("/listings/:id/edit", async (req, res) => {
+    let {id} = req.params;
+    const listingById = await Listing.findById(id);
+    console.log("Updated Listing ID: ", listingById)
+    res.render("edit.ejs", {listings : listingById});
+})
+
+app.put("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body});
+    res.redirect(`/listings/${id}`);
+})
 
 
-
-
+// Delete Listing
+app.delete("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    const deletedListing = await Listing.findByIdAndDelete(id);
+    console.log("Deleted Listing: ", deletedListing);
+    res.redirect("/listings");
+})
 
 
 
