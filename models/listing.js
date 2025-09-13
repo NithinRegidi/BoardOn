c4dddcc999d82f1bf125e661ae4e41b1727ffcd6
NithinRegidi <regidinithinsainaidu@gmail.com>
2025-09-13 10:35:19 +0530

    Add all cards and change in image schema with default

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const defaultImage = "https://images.unsplash.com/photo-1618773928121-c32242e63f39?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
 const listingSchema = new Schema({
     title: {
@@ -15,9 +16,13 @@ const listingSchema = new Schema({
         maxlength: 1000
     },
     image: {
-        default : "https://unsplash.com/photos/black-and-white-bed-near-brown-wooden-table-T5pL6ciEn-I",
+        // type: String,
+        // default: "https://unsplash.com/photos/black-and-white-bed-near-brown-wooden-table-T5pL6ciEn-I", // Correctly set default value here
+        // set: (v) => v === "" ? "https://unsplash.com/photos/black-and-white-bed-near-brown-wooden-table-T5pL6ciEn-I" : v,
+
         type: String,
-        set: (v) => v === "" ? "https://unsplash.com/photos/black-and-white-bed-near-brown-wooden-table-T5pL6ciEn-I" : v,
+        default: defaultImage, // Use the defaultImage constant
+        set: (v) => v.trim() === "" ? defaultImage : v, // Simplified set function
     },
     price: {
         type: Number,
